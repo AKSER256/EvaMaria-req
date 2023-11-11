b4e299f68f89aa16b5189fbd920b6655fcf12da1
Ak ser <133229616+AKSER256@users.noreply.github.com>
2023-11-11 18:36:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['23189612'])
+API_ID = int(environ[''])
 API_HASH = environ['71ed204544e0715e17ea033e0aca2d6f']
 BOT_TOKEN = environ['6177835537:AAGBXS0t9jmH0Y15wn2vE0fy5du6UR5LR_M']
 
